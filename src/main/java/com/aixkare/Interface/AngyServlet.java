04e92fa96be62fa3ffa993b44fc7f3fa30b583c4
Ángela López Mendoza <angelakimmichellle@hotmail.com>
2022-08-23 14:51:22 -0600

    Second commit... by Angy.

--- src/main/java/com/aixkare/Interface/AngyServlet.java
+++ src/main/java/com/aixkare/Interface/AngyServlet.java
@@ -23,7 +23,7 @@ public class AngyServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Angy!! served at: ").append(request.getContextPath());
+		response.getWriter().append("Angy!! ........................................................ served at: ").append(request.getContextPath());
 	}
 
 	/**
